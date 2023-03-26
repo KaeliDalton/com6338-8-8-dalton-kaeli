34db2d48ba38edf7a77670b2cbfe615c71861fd4
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-03-26 16:35:48 -0400

    initial variables

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code here
+// Global variables
+var API = "2df62c30dae6653493ac68c2bd19af8b"
+var weather = document.getElementById('weather')
+var form = document.querySelector('form')
+var search = document.getElementById('weather-search')
